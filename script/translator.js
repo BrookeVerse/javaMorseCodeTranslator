deb25221bb2d21692784c3627147ef6c052ed34d
BrookVerse <brookewaldorf90@gmail.com>
2022-09-05 08:41:23 +0100

    final commit

--- script/translator.js
+++ script/translator.js
@@ -1,8 +1,8 @@
 import { morseCode } from "../data/morseCode.js";
 import { morsetoEng } from "../data/morseToEng.js";
 
-/*This function is using the morseCode Object and making it an array, mapping that
-array to use the keys of morseCode to create an new array using the key values*/
+/*This function is using the string parameter and making it an array with no space
+,mapping that array to use a function so that the keys of morseCode to create an new array using the keys values*/
 export const translateToMorse = (string) => {
   return string
     .toLowerCase()
@@ -13,8 +13,8 @@ export const translateToMorse = (string) => {
     .join(" ");
 };
 
-/*This function is using the morseToEng Object and making it an array, mapping that
-array to use the keys of morseToEng to create an new array using the key values*/
+/*This function is using string and making it an array, mapping that
+array to use a function so that the keys of morseToEng to create an new array using the key values*/
 export const translateToEnglish = (string) => {
   return string
     .split(" ")
